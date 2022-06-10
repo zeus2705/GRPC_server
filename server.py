1ccee89812752f81eb342074d6572fed051fff34
zeus <antoine.remy99@gmail.com>
2022-06-10 15:13:00 +0200

    GRPC authenfication with SSL
    Signed-off-by: zeus <antoine.remy99@gmail.com>

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+
 import grpc
 import functionplusone_pb2
 import functionplusone_pb2_grpc
@@ -5,28 +6,39 @@ from concurrent import futures
 import sys, getopt
 
 
-#class contenant la fonction RPC
 class QuickMath(functionplusone_pb2_grpc.QuickMath):
-
-    #La function + 1
     def Plusone(self, request, context):
         print("\nPlusone was call with argument : ",str(request.x))
         return functionplusone_pb2.plusonereply(reply = request.x + 1)
 
-#Le variable du serveur
+
 server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
 
+def start():
+    try:
+        keyfile = ''
+        chainfile = ''
+        port = 26597
+        if keyfile or chainfile:
+            print("Starting the server with SSL/TLS on port " + str(port))
+            with open(keyfile, 'rb') as f:
+                private_key = f.read()
+            with open(sys.argv[2], 'rb') as f:
+                certificate_chain = f.read()
+            server_credentials = grpc.ssl_server_credentials( ( (private_key, certificate_chain) ) )
+            server.add_secure_port('127.0.0.1:' + str(port) , server_credentials)
+        else:
+            print("Starting the server without SSL/TLS on port " + str(port))
+            server.add_insecure_port('127.0.0.1:'+ str(port))
 
-#Bind le server au port 
-server.add_insecure_port('127.0.0.1:'+ '3266')
+        functionplusone_pb2_grpc.add_QuickMathServicer_to_server(QuickMath(), server)
+        server.start()
+        while True:
+            print("Server is running type exit to stop the server :", end='')
+            if input() == "exit":
+                break
+    except Exception as inst:
+        print(inst)
 
-#Bind la fonction RPC au server
-functionplusone_pb2_grpc.add_QuickMathServicer_to_server(QuickMath(), server)
-
-#Start le server
-server.start()
-
-while True:
-    print("Server is running type exit to stop the server :", end='')
-    if input() == "exit":
-        break
+start()
+    
